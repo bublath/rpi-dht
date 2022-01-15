68871d7fae26431c504e4aed1e67ffa58d1f32b2
Adimarantis <joerg.bublath@intel.com>
2022-01-15 15:30:38 +0100

    Making Debian build script independant of perl version

--- build-deb.sh
+++ build-deb.sh
@@ -4,15 +4,18 @@
 VER=`grep "^VERSION =" Makefile`
 set $VER
 VERSION=$3
+VER=`grep "^SITEARCHEXP =" Makefile`
+set $VER
+SITEARCHEXP=$3
 
 PACKAGE="RPi-DHT-perl_$VERSION-1_armhf"
 
 rm -r /tmp/$PACKAGE
-mkdir -p /tmp/$PACKAGE/usr/local/lib/arm-linux-gnueabihf/perl/5.28.1/RPi/
-mkdir -p /tmp/$PACKAGE/usr/local/lib/arm-linux-gnueabihf/perl/5.28.1/auto/RPi/DHT/
+mkdir -p /tmp/$PACKAGE/$SITEARCHEXP/RPi/
+mkdir -p /tmp/$PACKAGE/$SITEARCHEXP/auto/RPi/DHT/
 mkdir /tmp/$PACKAGE/DEBIAN
-cp lib/RPi/DHT.pm /tmp/$PACKAGE/usr/local/lib/arm-linux-gnueabihf/perl/5.28.1/RPi/
-cp blib/arch/auto/RPi/DHT/DHT.so /tmp/$PACKAGE/usr/local/lib/arm-linux-gnueabihf/perl/5.28.1/auto/RPi/DHT/
+cp lib/RPi/DHT.pm /tmp/$PACKAGE/$SITEARCHEXP/RPi/
+cp blib/arch/auto/RPi/DHT/DHT.so /tmp/$PACKAGE/$SITEARCHEXP/auto/RPi/DHT/
 cp DEBIAN/control /tmp/$PACKAGE/DEBIAN
 
 pushd /tmp
